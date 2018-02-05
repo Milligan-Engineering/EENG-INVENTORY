63ef4349be3ee9ba9c7b7cc45824a203eb39f3be
Sam Wehner <samwehner98@gmail.com>
2018-02-05 10:31:34 -0500

    Add project files.

---
+++ EENG INVENTORY/EENG INVENTORY.cpp
@@ -0,0 +1,78 @@
+//EENG INVENTORY
+//Sam Wehner
+//EENG 221
+//01/28/2017
+#include <iostream>
+#include <string>
+
+using namespace std;
+const int maxParts = 3;								
+int partNumber;
+string partName[maxParts];
+string partLoc[maxParts];
+int partQuant[maxParts];
+int partsRemoved;
+int menuChoice; 
+enum{ A=1, B=2};
+
+int main()
+{
+	//Part Name
+	partName[0] = " 1uF capacitor";
+	partName[1] = "resistor";
+	partName[2] = "transistor";
+
+	//Part Quantities
+	partQuant[0] = 51;
+	partQuant[1] = 73;
+	partQuant[2] = 85;
+
+	// Part Locations97\+
+	partLoc[0] = "0101A";
+	partLoc[1] = "0102A";
+	partLoc[2] = "0103A";
+	
+	cout << "What would you like to do today?\n\n";
+	cout << "To find a part press A and to exit the program press B";
+		cin >> menuChoice;
+	switch (menuChoice)
+	{
+	case 'A':
+			do 
+				{
+
+					cout << "Type part Number or 0 to exit:\n";   
+					cin >> partNumber;
+
+					if ((partNumber < 0) || (partNumber > maxParts)) //part not found
+					{
+						cout << "part not found.\n\n";
+					}
+
+
+
+					else // part found command string
+					{
+						cout << "part number: " << partNumber << " requested" <<"\n";
+						cout << "part name: " << partName[partNumber-1]<< "\n";
+						cout << "Quantity: " << partQuant[partNumber-1] << "pcs" << "\n";
+						cout << "Location: "<< partLoc[partNumber-1]<<"\n\n";
+						cout<< "How many parts are you removing? ";
+						cin >> partsRemoved;
+						 partQuant[partNumber-1] = partQuant[partNumber-1] - partsRemoved; //updates part quantity located in partQuant array
+						 cout << partQuant[partNumber - 1]<<" parts remaining\n\n";
+			 
+					}
+				}
+			while (partNumber != 0);
+				break;
+	case 'B':
+		
+		break;
+	}
+ 
+
+	
+
+	return 0;
+}
