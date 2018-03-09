e9d3e17efe946fe57222b30c5a2e6415c9f5fdc5
cjwilliamsMilligan <cjwilliams@my.milligan.edu>
2018-03-09 18:26:18 -0500

    Fix several little errors in code. Made style consistent.

--- EENG INVENTORY/EENG INVENTORY.cpp
+++ EENG INVENTORY/EENG INVENTORY.cpp
@@ -9,217 +9,200 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-//Function is good
 
 using namespace std;
 
-void partIndex(int partNumber[], string partName[], int partQuant[], string partLoc[]);//listprint
+void partIndex(string partType[], string partName[], int partQuant[], string partLoc[], const int maxParts); //listprint
 //Displays output of part numbers, Names, Quantities, and locations.
 //Only displays iformation for valid parts
 
-const int maxParts = 3;			
-
-//See if you can start getting rid of global variables.
-
-ifstream LABFILES;
-ifstream partInfo;
-ofstream partData;
-int partnumber;//search value
-int partNumber[maxParts];
-string partName[maxParts];
-string partLoc[maxParts];
-//string capacitor[3]; Moved out of global scope into int main
-//string resistor[3];
-//string transistor[3];
-int partQuant[maxParts];
-//int partsRemoved;
-
 int main()
 {
-		//Part types
-		//capacitor[3] = { "cap1", "cap2", "cap3" }; Doesn't work because you can't define multiple array elements except by intializing them like this:
-		string capacitor[] = { "cap1", "cap2", "cap3" }; //Works (notice there's no 3 in the index because arrays automatically size themselves when you initialize them
-		//resistor[3] = { "res1","res2", "res3"};
-		string resistor[] = { "res1", "res2", "res3" };
-		//transistor[3] = { "tran1", "tran2", "tran3" };
-		string transistor[] = { "tran1", "tran2", "tran3" };
+	const int maxParts = 3;
 
-		//Part Name
-		partName[0] = "capacitor";
-		partName[1] = "resistor";
-		partName[2] = "transistor";
+	ifstream LABFILES;
+	ifstream partInfo;
+	ofstream partData;
+	char nextChar;
+	string labFilesString = "";
+	string partType[maxParts];
+	string partName[maxParts];
+	int partQuant[maxParts];
+	string partLoc[maxParts];
+	int partNumber; //(partNumber-1) serves as an index to look up information for each part in the part arrays
 
-		//Part Quantities
-		partQuant[0] = 70;
-		partQuant[1] = 60;
-		partQuant[2] = 80;
+	//Part types
+	partType[0] = "capacitor";
+	partType[1] = "resistor";
+	partType[2] = "transistor";
+	//Part names
+	partName[0] = "100uF electrolytic";
+	partName[1] = "1.5kOhm, 0.25W carbon film";
+	partName[2] = "2N5401 PNP silicon";
+	//Part quantities
+	partQuant[0] = 70;
+	partQuant[1] = 60;
+	partQuant[2] = 80;
+	//Part locations
+	partLoc[0] = "0101A";
+	partLoc[1] = "0102A";
+	partLoc[2] = "0103A";
 
-		// Part Locations
-		partLoc[0] = "0101A";
-		partLoc[1] = "0102A";
-		partLoc[2] = "0103A";
+	cout << "Let's say:\n";
+	cout << "----------\n";
+	cout << "part 1 is a 100uF, 25V electrolytic capacitor, with quantity of 70, in location 0101A\n\n";
+	cout << "part 2 is a 1.5kOhm, 0.25W carbon film resistor, with quantity 60, in location 0102A\n\n";
+	cout << "part 3 is a 2N5401 PNP silicon transistor, with quantity 80, in location 0103A\n\n\n";
 		
+	char menuChoice;
+	do
+	{
+		cout << endl;
 		cout << "What would you like to do today? \n\n";
-		cout << "To find a part press P:\n" << "To access part list press I: \n" << "To find class index press S:\n "<<"To exit press E:\n"<<"To get Lab instructions press L:\n";
-		char menuChoice;
+		cout << "To find a part press P\n";
+		cout << "To access part list press I \n";
+		cout << "To find class index press S\n";
+		cout << "To get lab instructions press L\n";
+		cout << "To exit press E\n\n";
+
 		cin >> menuChoice;
-		do
+		switch (menuChoice)
 		{
-
-			switch (menuChoice)
+		case 'P':
+		case 'p':
+			do
 			{
-			case 'P':
-			case'p':
+				int partsRemoved;
+				cout << endl << "Type part number or 0 to exit to main menu: ";
+				cin >> partNumber;
 
-				do
+				if ((partNumber < 0) || (partNumber > maxParts))
 				{
-					//phasing out Global variables
-					int partsRemoved;
-					cout << "Type part Number or E to exit:\n";
-					cin >> partnumber;
-
-					if ((partnumber <= 0) || (partnumber > maxParts)) //part not found
-					{
-						cout << "part not found.\n\n";
-
-						if (partnumber = 'e', 'E')
-						{
-							return(0);
-						}
-
-					}
-					
-					else // part found command string
-					{
-						int choice;
-						cout << "part number: " << partnumber << " requested" << "\t";
-						cout << "part name: " << partName[partnumber - 1] << "\t";
-						cout << "Quantity: " << partQuant[partnumber - 1] << "pcs" << "\t\t";
-						cout << "Location: " << partLoc[partnumber - 1] << "\n\n";
-						cout << "How many parts are you removing? ";
-						cin >> partsRemoved;
-						partQuant[partnumber - 1] = partQuant[partnumber - 1] - partsRemoved; //updates part quantity located in partQuant array
-						cout << partQuant[partnumber - 1] << " parts remaining\n\n";
-						cout << "would you like to save this part? ";
-
-						cin >> choice;
-						if (choice = 1)
-						{
-							partData.open("Datasheet.txt");
-							if (partData.fail())
-							{
-								cout << " file failed to open";
-								char wait;
-								cin >> wait;
-								exit(1);
-							}
-							partData << partnumber << partName[partnumber - 1] << partQuant[partnumber - 1] << partLoc[partnumber - 1];
-							partData.close();
-							//partData << "part sheet ";
-							//exit(1);
-						}
-					}
-					/*if (partnumber = 'e', 'E') 
-					{
-						return(0);
-					}*/
-				} 
-				while (partnumber != 0);
-				break;
-
-			case 'I':
-			case'i':
-				partIndex(partNumber, partName, partQuant, partLoc);
-
-				break;
-
-			case'C': //"part check" option
-			case'c'://quantitySort(int partQuant, int Order, int quant);
-
-				break;
-
-			case 'E':
-			case 'e':
-
-				return(0);
-				break;
-
-			case 'S':
-			case 's':
-				// specific part search
-				break;
-
-			case'L':
-			case'l':
-				LABFILES.open("LAB1.txt"); //this option allows for the user to acess the labs
-				if (LABFILES.fail())
-				{
-					cout << "Lab file failed to open";
-					char wait;
-					cin >> wait;
-					exit(1);
+					cout << "Part not found.\n\n";
 				}
-
-				char cstring[30];
-				for (int i = 0; i < 30; i++)
+				else if (partNumber != 0) //Part found
 				{
-					LABFILES.get(cstring[i]);
-					cout << cstring<<endl;
+					cout << endl;
+					cout << "Part Number " << partNumber << " requested" << "\n";
+					cout << "Type: " << partType[partNumber - 1] << "\n";
+					cout << "Name: " << partName[partNumber - 1] << " " << partType[partNumber - 1] << "\n";
+					cout << "Quantity: " << partQuant[partNumber - 1] << " pcs" << "\n";
+					cout << "Location: " << partLoc[partNumber - 1] << "\n\n";
+
+					//add a check here to make sure that partsRemoved is not greater than the number of parts in stock - Casey
+					cout << "How many parts are you removing? ";
+					cin >> partsRemoved;
+					partQuant[partNumber - 1] = partQuant[partNumber - 1] - partsRemoved; //updates part quantity located in partQuant array
+					cout << partQuant[partNumber - 1] << " parts remaining\n\n";
+
+					int choice;
+					cout << "Type 1 if you would like to save this part information: ";
+					cin >> choice;
+					if (choice == 1) //had (choice = 1), cause infinite loop - Casey
+					{
+						partData.open("Datasheet.txt");
+						if (partData.fail())
+						{
+							cout << "File failed to open";
+							char wait;
+							cin >> wait;
+							exit(1);
+						}
+						partData << partNumber << " " << partName[partNumber - 1] << " " << partType[partNumber - 1]
+							<< " " << partQuant[partNumber - 1] << " " << partLoc[partNumber - 1];
+						partData.close();
+					}
 				}
-					
+			} while (partNumber != 0);
+			break;
+
+		case 'I':
+		case 'i':
+			partIndex(partName, partType, partQuant, partLoc, maxParts);
+			break;
+
+		case 'C':
+		case 'c':
+			//"part check" option
+			//quantitySort(int partQuant, int Order, int quant);
+			break;
+
+		case 'E':
+		case 'e':
+			break;
+
+		case 'S':
+		case 's':
+			//Specific part search
+			break;
+
+		case 'L':
+		case 'l':
+			LABFILES.open("LAB1.txt"); //this option allows for the user to acess the labs
+			if (LABFILES.fail())
+			{
+				cout << "Lab file failed to open";
 				char wait;
 				cin >> wait;
-				LABFILES.close();
-				break;
+				exit(1);
 			}
 
-			/*	{
-					//char menuChoice;
-					cout <<"press M to return to the menu\n"<< " or press E to exit\n ";
-						switch (menuChoice)
-						{
-						case'E':
-						case'e':
-								return(0);
-						}
-				}
-				*///Tell user to enter any character to exit program.
-			char wait;
-			cin >> wait;
-		} while ((menuChoice !='E')||(menuChoice !='e'));
+			LABFILES.get(nextChar);
+			do
+			{
+				labFilesString += nextChar;
+				LABFILES.get(nextChar);
+			} while (nextChar != '\n'); //Continue until new-line character
 
+			cout << labFilesString << endl;
+
+			LABFILES.close();
+
+			//I don't think there's any point in having the wait here now - Casey		
+			//char wait;
+			//cin >> wait;
+			break;
+
+		//default:
+
+			//cout << "Not a valid menu choice.\n\n";
+		}
+
+	} while ((menuChoice != 'E') && (menuChoice != 'e')); //was an OR operator, corrected to AND - Casey
+
+	//I don't think there's any point in having the wait here now - Casey
+	//char wait;
+	//cin >> wait;
 	return 0;
 }
 
-void partIndex(int partNumber[], string partName[], int partQuant[], string partLoc[])//listprint
-// prints out part list
+void partIndex(string partName[], string partType[], int partQuant[], string partLoc[], const int maxParts)
 {
-	int i;
-	for (i = 0; i < maxParts; i++)
+	for (int i = 1; i <= maxParts; i++)
 	{
+		//see pages 325-331 in text to get this to format "prettier" - Casey
+		cout << endl;
 		cout << "Part Number: " << i << "\t";
-		cout << "Part Name: " << partName[i] << "\t";
-		cout << "Part Quantity: " << partQuant[i] << "\t";
-		cout << "Part Location: " << partLoc[i] << "\t""\n\n";
+		cout << "Part Type: " << partType[i - 1] << "\t";
+		cout << "Part Name: " << partName[i - 1] << " " << partType[i - 1] << "\t";
+		cout << "Part Quantity: " << partQuant[i - 1] << "\t";
+		cout << "Part Location: " << partLoc[i - 1] << "\t\n\n";
 	}
-	
 }
 
- //void quantitySort(int partQuant[], int Order[], int quant)
-//{
-	//int temp;
-	//for (int i = 0; i < quant - 1; i++)
-//	{
-		//for (int j = 0; j < quant - i - 1; j++)
-//		{
-//			if (partQuant[Order[j]] > partQuant[Order[j + 1]])
-//			{
-//				temp = Order[j];
-//				Order[j] = Order[j + 1];
-//				Order[j + 1] = temp;
-//			}
-//		}
-//	}
-
-
-
+/*void quantitySort(int partQuant[], int Order[], int quant)
+{
+	int temp;
+	for (int i = 0; i < quant - 1; i++)
+	{
+		for (int j = 0; j < quant - i - 1; j++)
+		{
+			if (partQuant[Order[j]] > partQuant[Order[j + 1]])
+			{
+				temp = Order[j];
+				Order[j] = Order[j + 1];
+				Order[j + 1] = temp;
+			}
+		}
+	}
+}*/
